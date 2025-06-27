30a5b0d2f39e8b2bd983b94eba7161d50c51dc3b
nullsnap <callumbritton20@icloud.com>
2025-06-27 22:12:09 +0100

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+// includes/footer.php
+?>
+    <script>
+        // Simple auto-refresh every 60 seconds
+        setTimeout(() => {
+            window.location.reload();
+        }, 60000);
+    </script>
+</body>
+</html>
